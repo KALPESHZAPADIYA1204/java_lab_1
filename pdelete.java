6b49269f8057abce51fbdeb27284cfebbf5cbd7b
KALPESHZAPADIYA1204 <bca2022kalpesh1682@tnraocollege.org>
2024-07-22 17:49:24 +0530

    Add files via upload

---
+++ pdelete.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+public class Pdelete{
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String url = "jdbc:mysql://localhost:3306/company";
+
+        try {
+        
+            conn = DriverManager.getConnection(url,"root","tnrao");
+             String sql = "DELETE FROM emp WHERE Firstname = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "kalpesh");
+            int val = pstmt.executeUpdate();
+             System.out.println(val + " record(s) deleted successfully");
+
+            pstmt.close();
+            conn.close();
+        } 
+        catch (Exception e)
+         {
+            e.printStackTrace();
+        }
+    }
+}
